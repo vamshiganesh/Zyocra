0f99faedc0f07cc3af4e14bd6bf118b172dcc240
Vamshi Ganesh <vamshiganesh27@gmail.com>
2026-04-20 12:00:00 +0000

    Tweaking frontend: Landing Page - 18

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/fonts.css";
 import "./styles/global.css";
+import "./styles/scrollbar.css";
 import "./styles/layout.css";
 
 createRoot(document.getElementById("root")!).render(
